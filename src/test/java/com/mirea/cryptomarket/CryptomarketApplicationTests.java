2270e3a5668fc81a16381ed45e9e58218bd621f8
Danila Kalugin <danilakalugin@yandex.ru>
2023-02-20 16:56:06 +0300

    Initial commit

---
+++ src/test/java/com/mirea/cryptomarket/CryptomarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mirea.cryptomarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptomarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
